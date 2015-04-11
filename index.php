c0b6ca8707a9430fba6d21585155a4eb3c8cc615
litalbarnoy <litalbarnoy5@gmail.com>
2015-04-11 16:27:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 hi zibi
+
+<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/litalbarnoy.lmjhji6l/attribution,zoompan,zoomwheel,geocoder,share.html?access_token=pk.eyJ1IjoibGl0YWxiYXJub3kiLCJhIjoiazh2OTdFYyJ9.66NbqOtfKhEV7d8fdbMqWw'></iframe>
