29303444695f8c472e3e8826c08e3f99782c7160
litalbarnoy <litalbarnoy5@gmail.com>
2015-04-11 16:31:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-hi zibi
-
-<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/litalbarnoy.lmjhji6l/attribution,zoompan,zoomwheel,geocoder,share.html?access_token=pk.eyJ1IjoibGl0YWxiYXJub3kiLCJhIjoiazh2OTdFYyJ9.66NbqOtfKhEV7d8fdbMqWw'></iframe>
+<!DOCTYPE>
+<html>
+  <head>
+    <meta charset="utf-8 />
+    <title> Indie Mapbox </title>
+  </head>
+  <body>
+    <h1> hi </h1> 
+  </body>
+</html>
